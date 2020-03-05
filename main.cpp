4b330b466c4adc4cc063fa36127685a8623631e5
Alena Gracheva <36485363+Aquarium42@users.noreply.github.com>
2020-03-05 23:12:41 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
+#include <unordered_map>
+#include <streambuf>
 #include <iostream>
-#include <cstring>
-
-
+#include <MiniJava.lexer.h>
 
 int main() {
     yyscan_t scanner;
-    YY_BUFFER_STATE buf;
-    yylex_init(&scanner);
-    buf = yy_scan_string("replace me with the string youd like to scan", scanner);
-    yylex(scanner);
-    yy_delete_buffer(buf, scanner);
-    yylex_destroy(scanner);
+    if (yylex_init(&scanner)) {
+        throw std::runtime_error("Failed initialization scanner");
+    }
+
+    YY_BUFFER_STATE state = yy_scan_string("down down down", scanner);
+
+    std::cout << yylex(scanner);
+
+
+
+
+    /*
+    Lexer lexer;
+    while (true) {
+        int token = lexer.ScanToken();
+        if (token == TOKEN_EXIT) {
+            std::cout << "Good bye" << std::endl;
+            return 0;
+        }
+        std::cout << token << std::endl;
+    }*/
     return 0;
 }
